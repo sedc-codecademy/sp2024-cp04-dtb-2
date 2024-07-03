80d83e5c94d0df22cdaa24b9cea794f84418d3b1
TheForis <boriskrstovski@gmail.com>
2024-07-04 00:30:52 +0200

    Update themeToggle.js
    fixed dark mode//
    filter remained white while dark mode is pressed

--- source/js/modules/themeToggle.js
+++ source/js/modules/themeToggle.js
@@ -5,8 +5,8 @@ document.getElementById("lightDarkToggle").addEventListener("click", function(){
         document.getElementById("srcIcon").setAttribute("src","./source/data/icons/search.svg");
         document.getElementById("filterIcon").setAttribute("src","./source/data/icons/filter.svg");
         document.getElementById("lightDarkToggle").setAttribute("src","./source/data/icons/brightness-high-fill.svg");
-        document.getElementById("navAccIcon").setAttribute("class", "btn btn-dark");
-        document.getElementById("dropdownMenuButton").setAttribute("style", "background-color: white; border: 0; color: black;")
+        document.getElementById("loginBtn").setAttribute("class", "btn btn-dark");
+        document.getElementById("dropdownMenuButton").setAttribute("style", "background-color: white; color: black;")
         document.getElementById("tagFilterDropdown").setAttribute("style","background-color: white; color: black;" );
 
     } else {
@@ -14,8 +14,8 @@ document.getElementById("lightDarkToggle").addEventListener("click", function(){
         document.getElementById("srcIcon").setAttribute("src","./source/data/icons/searchWhite.svg");
         document.getElementById("filterIcon").setAttribute("src","./source/data/icons/filterWhite.svg");
         document.getElementById("lightDarkToggle").setAttribute("src","./source/data/icons/moon.svg");
-        document.getElementById("navAccIcon").setAttribute("class", "btn btn-light");
-        document.getElementById("dropdownMenuButton").setAttribute("style", "background-color: #212529; border: 0; color: white;");
+        document.getElementById("loginBtn").setAttribute("class", "btn btn-light");
+        document.getElementById("dropdownMenuButton").setAttribute("style", "background-color: #212529; color: white;");
         document.getElementById("tagFilterDropdown").setAttribute("style","background-color: #212529; color: white;" );
     }
 })  
