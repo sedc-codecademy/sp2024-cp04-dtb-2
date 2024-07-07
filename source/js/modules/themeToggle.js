f3f124d7a9d4470663c05b2a36646a20ae730949
Dan23P <149869797+Dan23P@users.noreply.github.com>
2024-07-07 14:47:03 +0200

    Update themeToggle.js

--- source/js/modules/themeToggle.js
+++ source/js/modules/themeToggle.js
@@ -1,25 +1,25 @@
 console.log("CONNECTED");
 document.getElementById("lightDarkToggle").addEventListener("click", function(){ // ligth Theme 
     if (document.documentElement.getAttribute("data-bs-theme") == "dark") {
-        document.documentElement.setAttribute("data-bs-theme", "dark"); //dark Theme 
-        document.getElementById("newsletterImg").setAttribute("src","./source/data/icons/envelopeWhite.svg");
-        document.getElementById("srcIcon").setAttribute("src","./source/data/icons/searchWhite.svg");
-        document.getElementById("filterIcon").setAttribute("src","./source/data/icons/filterWhite.svg");
-        document.getElementById("lightDarkToggle").setAttribute("src","./source/data/icons/moon.svg");
-        document.getElementById("loginBtn").setAttribute("class", "btn btn-light");
-        document.getElementById("dropdownMenuClickableInside").setAttribute("style", "color: white;");
+        document.documentElement.setAttribute("data-bs-theme", "light"); //dark Theme 
+        document.getElementById("newsletterImg").setAttribute("src","./source/data/icons/envelope.svg");
+        document.getElementById("srcIcon").setAttribute("src","./source/data/icons/search.svg");
+        document.getElementById("filterIcon").setAttribute("src","./source/data/icons/filter.svg");
+        document.getElementById("lightDarkToggle").setAttribute("src","./source/data/icons/brightness-high-fill.svg");
+        document.getElementById("loginBtn").setAttribute("class", "btn btn-dark");
+        document.getElementById("dropdownMenuClickableInside").setAttribute("style", "color: black;");
         document.querySelectorAll(".starsIcon").forEach(function(element){
-            element.setAttribute("src","./source/data/icons/starWhite.svg");
+            element.setAttribute("src","./source/data/icons/star.svg");
         });
         document.querySelectorAll(".commentsIcon").forEach(function(element){
-            element.setAttribute("src","./source/data/icons/chat-rightWhite.svg");
+            element.setAttribute("src","./source/data/icons/chat-right.svg");
         });
         document.querySelectorAll(".form-check-label").forEach(function(element) {
-            element.style.color = "white";
+            element.style.color = "black";
         });
         document.querySelectorAll('.card, .card-title').forEach(function(card) {
-            card.style.backgroundColor = 'lightblue'; 
-            card.style.color = "white";
+            card.style.backgroundColor = '#f0efef'; 
+            card.style.color = "black";
         });
         document.getElementById("")
     }
